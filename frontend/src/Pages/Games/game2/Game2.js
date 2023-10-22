2b8a2212f1e0875db0278e1022584359e3d71b4c
Adrian Laje <67974071+adrianrd7@users.noreply.github.com>
2023-10-21 19:35:12 -0500

    juego 2 y correciones generelaes

---
+++ frontend/src/Pages/Games/game2/Game2.js
@@ -0,0 +1,91 @@
+import React, { useState, useEffect } from 'react';
+import Swal from 'sweetalert2';
+import { DndProvider } from 'react-dnd';
+import { HTML5Backend } from 'react-dnd-html5-backend';
+import './game2.css';
+import { getWords } from '../../../services/game2Service';
+import altavoz from "../../../assets/altavoz.png"
+
+const Game2 = () => {
+    const [word, setWord] = useState(null);
+    const [showPlayAgainButton, setShowPlayAgainButton] = useState(false);
+
+    const getWord = () => {
+        const words = getWords();
+        const randomIndex = Math.floor(Math.random() * words.length);
+        const randomWord = words[randomIndex];
+        setWord(randomWord);
+    }
+
+    useEffect(() => {
+        getWord()
+    }, []);
+
+    const speak = () => {
+        if (word) {
+            const syllables = word.syllable_separation.split('-');
+            
+            const speechParts = syllables.map((syllable, index) => {
+                const utterance = new SpeechSynthesisUtterance(syllable);
+                utterance.pause = index < syllables.length - 1 ? 100 : 0;
+                return utterance;
+            });
+    
+            speechParts.forEach(part => {
+                window.speechSynthesis.speak(part);
+            });
+        }
+    }
+    
+
+
+
+
+    const handleDrop = (droppedLetter) => {
+        if (droppedLetter === word.word) {
+            Swal.fire({
+                icon: 'success',
+                title: 'Correcto',
+                text: `¡La palabra "${word.word}" es correcta!`,
+            });
+            setShowPlayAgainButton(true);
+        } else {
+            Swal.fire({
+                icon: 'error',
+                title: 'Incorrecto',
+                text: 'La letra soltada no coincide con la palabra.',
+            });
+        }
+    }
+
+    return (
+        <div>
+            <DndProvider backend={HTML5Backend}>
+                <div className="targetWord">targetWord</div>
+                <div className="center">
+                    <div className="title">
+                        {word ? word.word : ''}
+                    </div>
+                    <div className="speaker-button">
+                        <img src={altavoz} className="altavoz-btn" onClick={speak} alt='altavoz' />
+                    </div>
+                    <div className="syllable-separation">
+                        <h2>Separación de sílabas</h2>
+                        <h5>{word ? word.syllable_separation : ''}</h5>
+                    </div>
+                </div>
+                {!showPlayAgainButton ? (
+                    <div
+                        className="piece"
+                        onDragStart={(e) => e.dataTransfer.setData('word', word ? word.word : '')}
+                        draggable
+                    >
+                        <span>{word ? word.word : ''}</span>
+                    </div>
+                ) : <button onClick={getWord}>Jugar Nuevamente</button>}
+            </DndProvider>
+        </div>
+    );
+};
+
+export default Game2;
---
+++ frontend/src/services/game2Service.js
@@ -0,0 +1,429 @@
+export const getWords =  () => {
+    return  [
+        {
+            word: "manzana",
+            syllable_separation: "man-za-na",
+            imageURL: "http://imagen_manzana.com",
+        },
+        {
+            word: "perro",
+            syllable_separation: "per-ro",
+            imageURL: "http://imagen_perro.com",
+        },
+        {
+            word: "gato",
+            syllable_separation: "ga-to",
+            imageURL: "http://imagen_gato.com",
+        },
+        {
+            word: "fresa",
+            syllable_separation: "fre-sa",
+            imageURL: "http://imagen_fresa.com",
+        },
+        {
+            word: "zanahoria",
+            syllable_separation: "za-na-ho-ria",
+            imageURL: "http://imagen_zanahoria.com",
+        },
+        {
+            word: "león",
+            syllable_separation: "le-ón",
+            imageURL: "http://imagen_leon.com",
+        },
+        {
+            word: "kiwi",
+            syllable_separation: "ki-wi",
+            imageURL: "http://imagen_kiwi.com",
+        },
+        {
+            word: "elefante",
+            syllable_separation: "e-le-fan-te",
+            imageURL: "http://imagen_elefante.com",
+        },
+        {
+            word: "uva",
+            syllable_separation: "u-va",
+            imageURL: "http://imagen_uva.com",
+        },
+        {
+            word: "naranja",
+            syllable_separation: "na-ran-ja",
+            imageURL: "http://imagen_naranja.com",
+        },
+        {
+            word: "tigre",
+            syllable_separation: "ti-gre",
+            imageURL: "http://imagen_tigre.com",
+        },
+        {
+            word: "sandía",
+            syllable_separation: "san-dí-a",
+            imageURL: "http://imagen_sandia.com",
+        },
+        {
+            word: "oso",
+            syllable_separation: "o-so",
+            imageURL: "http://imagen_oso.com",
+        },
+        {
+            word: "maní",
+            syllable_separation: "ma-ní",
+            imageURL: "http://imagen_mani.com",
+        },
+        {
+            word: "jirafa",
+            syllable_separation: "ji-ra-fa",
+            imageURL: "http://imagen_jirafa.com",
+        },
+        {
+            word: "tomate",
+            syllable_separation: "to-ma-te",
+            imageURL: "http://imagen_tomate.com",
+        },
+        {
+            word: "lobo",
+            syllable_separation: "lo-bo",
+            imageURL: "http://imagen_lobo.com",
+        },
+        {
+            word: "pera",
+            syllable_separation: "pe-ra",
+            imageURL: "http://imagen_pera.com",
+        },
+        {
+            word: "pavo",
+            syllable_separation: "pa-vo",
+            imageURL: "http://imagen_pavo.com",
+        },
+        {
+            word: "ciruela",
+            syllable_separation: "ci-ru-e-la",
+            imageURL: "http://imagen_ciruela.com",
+        },
+        {
+            word: "abeja",
+            syllable_separation: "a-be-ja",
+            imageURL: "http://imagen_abeja.com",
+        },
+        {
+            word: "pollo",
+            syllable_separation: "po-llo",
+            imageURL: "http://imagen_pollo.com",
+        },
+        {
+            word: "calabaza",
+            syllable_separation: "ca-la-ba-za",
+            imageURL: "http://imagen_calabaza.com",
+        },
+        {
+            word: "serpiente",
+            syllable_separation: "ser-pien-te",
+            imageURL: "http://imagen_serpiente.com",
+        },
+        {
+            word: "manzano",
+            syllable_separation: "man-za-no",
+            imageURL: "http://imagen_manzano.com",
+        },
+        {
+            word: "cocodrilo",
+            syllable_separation: "co-co-dri-lo",
+            imageURL: "http://imagen_cocodrilo.com",
+        },
+        {
+            word: "uvas",
+            syllable_separation: "u-vas",
+            imageURL: "http://imagen_uvas.com",
+        },
+        {
+            word: "frijoles",
+            syllable_separation: "fri-jo-les",
+            imageURL: "http://imagen_frijoles.com",
+        },
+        {
+            word: "colibrí",
+            syllable_separation: "co-li-brí",
+            imageURL: "http://imagen_colibri.com",
+        },
+        {
+            word: "manzanas",
+            syllable_separation: "man-za-nas",
+            imageURL: "http://imagen_manzanas.com",
+        },
+        {
+            word: "ratón",
+            syllable_separation: "ra-tón",
+            imageURL: "http://imagen_raton.com",
+        },
+        {
+            word: "papaya",
+            syllable_separation: "pa-pa-ya",
+            imageURL: "http://imagen_papaya.com",
+        },
+        {
+            word: "tiburón",
+            syllable_separation: "ti-bu-rón",
+            imageURL: "http://imagen_tiburon.com",
+        },
+        {
+            word: "manzanos",
+            syllable_separation: "man-za-nos",
+            imageURL: "http://imagen_manzanos.com",
+        },
+        {
+            word: "avión",
+            syllable_separation: "a-vión",
+            imageURL: "http://imagen_avion.com",
+        },
+        {
+            word: "cereza",
+            syllable_separation: "ce-re-za",
+            imageURL: "http://imagen_cereza.com",
+        },
+        {
+            word: "caballo",
+            syllable_separation: "ca-ba-llo",
+            imageURL: "http://imagen_caballo.com",
+        },
+        {
+            word: "banana",
+            syllable_separation: "ba-na-na",
+            imageURL: "http://imagen_banana.com",
+        },
+        {
+            word: "manzanas",
+            syllable_separation: "man-za-nas",
+            imageURL: "http://imagen_manzanas.com",
+        },
+        {
+            word: "ardilla",
+            syllable_separation: "ar-dil-la",
+            imageURL: "http://imagen_ardilla.com",
+        },
+        {
+            word: "espinaca",
+            syllable_separation: "es-pi-na-ca",
+            imageURL: "http://imagen_espinaca.com",
+        },
+        {
+            word: "pájaro",
+            syllable_separation: "pá-ja-ro",
+            imageURL: "http://imagen_pajaro.com",
+        },
+        {
+            word: "sardina",
+            syllable_separation: "sar-di-na",
+            imageURL: "http://imagen_sardina.com",
+        },
+        {
+            word: "pimiento",
+            syllable_separation: "pi-mien-to",
+            imageURL: "http://imagen_pimiento.com",
+        },
+        {
+            word: "murciélago",
+            syllable_separation: "mur-cié-la-go",
+            imageURL: "http://imagen_murcielago.com",
+        },
+        {
+            word: "cebolla",
+            syllable_separation: "ce-bo-lla",
+            imageURL: "http://imagen_cebolla.com",
+        },
+        {
+            word: "ballena",
+            syllable_separation: "ba-lle-na",
+            imageURL: "http://imagen_ballena.com",
+        },
+        {
+            word: "alcachofa",
+            syllable_separation: "al-ca-cho-fa",
+            imageURL: "http://imagen_alcachofa.com",
+        },
+        {
+            word: "tucán",
+            syllable_separation: "tu-cán",
+            imageURL: "http://imagen_tucan.com",
+        },
+        {
+            word: "pistacho",
+            syllable_separation: "pis-ta-cho",
+            imageURL: "http://imagen_pistacho.com",
+        },
+        {
+            word: "pepino",
+            syllable_separation: "pe-pi-no",
+            imageURL: "http://imagen_pepino.com",
+        },
+        {
+            word: "iguana",
+            syllable_separation: "i-gua-na",
+            imageURL: "http://imagen_iguana.com",
+        },
+        {
+            word: "cucaracha",
+            syllable_separation: "cu-ca-ra-cha",
+            imageURL: "http://imagen_cucaracha.com",
+        },
+        {
+            word: "paraguas",
+            syllable_separation: "pa-ra-guas",
+            imageURL: "http://imagen_paraguas.com",
+        },
+        {
+            word: "espagueti",
+            syllable_separation: "es-pa-gue-ti",
+            imageURL: "http://imagen_espagueti.com",
+        },
+        {
+            word: "gallina",
+            syllable_separation: "ga-lli-na",
+            imageURL: "http://imagen_gallina.com",
+        },
+        {
+            word: "cebollas",
+            syllable_separation: "ce-bo-llas",
+            imageURL: "http://imagen_cebollas.com",
+        },
+        {
+            word: "arándano",
+            syllable_separation: "a-rán-da-no",
+            imageURL: "http://imagen_arandano.com",
+        },
+        {
+            word: "zapato",
+            syllable_separation: "za-pa-to",
+            imageURL: "http://imagen_zapato.com",
+        },
+        {
+            word: "papagayo",
+            syllable_separation: "pa-pa-ga-yo",
+            imageURL: "http://imagen_papagayo.com",
+        },
+        {
+            word: "esmeralda",
+            syllable_separation: "es-me-ral-da",
+            imageURL: "http://imagen_esmeralda.com",
+        },
+        {
+            word: "orquídea",
+            syllable_separation: "or-quí-de-a",
+            imageURL: "http://imagen_orquidea.com",
+        },
+        {
+            word: "caballos",
+            syllable_separation: "ca-ba-llos",
+            imageURL: "http://imagen_caballos.com",
+        },
+        {
+            word: "almendras",
+            syllable_separation: "al-men-dras",
+            imageURL: "http://imagen_almendras.com",
+        },
+        {
+            word: "agricultor",
+            syllable_separation: "a-gri-cul-tor",
+            imageURL: "http://imagen_agricultor.com",
+        },
+        {
+            word: "girasol",
+            syllable_separation: "gi-ra-sol",
+            imageURL: "http://imagen_girasol.com",
+        },
+        {
+            word: "búho",
+            syllable_separation: "bú-ho",
+            imageURL: "http://imagen_buho.com",
+        },
+        {
+            word: "almohada",
+            syllable_separation: "al-mo-ha-da",
+            imageURL: "http://imagen_almohada.com",
+        },
+        {
+            word: "avellana",
+            syllable_separation: "a-ve-lla-na",
+            imageURL: "http://imagen_avellana.com",
+        },
+        {
+            word: "flamenco",
+            syllable_separation: "fla-men-co",
+            imageURL: "http://imagen_flamenco.com",
+        },
+        {
+            word: "cocina",
+            syllable_separation: "co-ci-na",
+            imageURL: "http://imagen_cocina.com",
+        },
+        {
+            word: "pelícano",
+            syllable_separation: "pe-lí-ca-no",
+            imageURL: "http://imagen_pelicano.com",
+        },
+        {
+            word: "almejas",
+            syllable_separation: "al-me-jas",
+            imageURL: "http://imagen_almejas.com",
+        },
+        {
+            word: "computadora",
+            syllable_separation: "com-pu-ta-do-ra",
+            imageURL: "http://imagen_computadora.com",
+        },
+        {
+            word: "coliflor",
+            syllable_separation: "co-li-flor",
+            imageURL: "http://imagen_coliflor.com",
+        },
+        {
+            word: "zapatos",
+            syllable_separation: "za-pa-tos",
+            imageURL: "http://imagen_zapatos.com",
+        },
+        {
+            word: "cacahuate",
+            syllable_separation: "ca-ca-hua-te",
+            imageURL: "http://imagen_cacahuate.com",
+        },
+        {
+            word: "abejorro",
+            syllable_separation: "a-be-jor-ro",
+            imageURL: "http://imagen_abejorro.com",
+        },
+        {
+            word: "fotografía",
+            syllable_separation: "fo-to-gra-fí-a",
+            imageURL: "http://imagen_fotografia.com",
+        },
+        {
+            word: "burro",
+            syllable_separation: "bu-rro",
+            imageURL: "http://imagen_burro.com",
+        },
+        {
+            word: "mariposa",
+            syllable_separation: "ma-ri-po-sa",
+            imageURL: "http://imagen_mariposa.com",
+        },
+        {
+            word: "esmeraldas",
+            syllable_separation: "es-me-ral-das",
+            imageURL: "http://imagen_esmeraldas.com",
+        },
+        {
+            word: "paracaídas",
+            syllable_separation: "pa-ra-ca-í-das",
+            imageURL: "http://imagen_paracaidas.com",
+        },
+        {
+            word: "cachorro",
+            syllable_separation: "ca-cho-rro",
+            imageURL: "http://imagen_cachorro.com",
+        },
+        {
+            word: "computadoras",
+            syllable_separation: "com-pu-ta-do-ras",
+            imageURL: "http://imagen_computadoras.com",
+        },
+    ];
+}
