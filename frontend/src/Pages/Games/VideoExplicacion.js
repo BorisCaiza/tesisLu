21a03cdb4474ae1795aaa193de1335f21a9fb05f
Adrian Laje <67974071+adrianrd7@users.noreply.github.com>
2023-12-08 13:48:22 -0500

    agregado videos explicativos

--- frontend/src/Pages/Games/VideoExplicacion.js
+++ frontend/src/Pages/Games/VideoExplicacion.js
@@ -7,7 +7,7 @@ import fondo10 from '../../assets/images/Fondos/fondo_10.png';
 import fondo11 from '../../assets/images/Fondos/fondo_11.png';
 import fondo12 from '../../assets/images/Fondos/fondo_12.png';
 
-const backgrounds = [fondo9, fondo10, fondo11, fondo12 ];
+const backgrounds = [fondo9, fondo10, fondo11, fondo12];
 
 function VideoExplicacion() {
     const { id } = useParams()
@@ -27,25 +27,44 @@ function VideoExplicacion() {
         game3: "Conciencia de rima",
         game4: "Juego de Memoria"
     };
+
+    const videoUrls = {
+        game1: "https://drive.google.com/uc?id=1ImahWVbBxNdKEVHdqT7T8V16phPtMdvA",
+        game2: "https://drive.google.com/uc?id=1vuQsso1vGu9yIp7CvgEROF54TgW5AFO9",
+        game3: "https://drive.google.com/uc?id=1Jt4XXMdrmWmUBm3CjEMX_zbWymovejgg",
+        game4: "https://drive.google.com/uc?id=1ieP3K7S-Va-44JFPPwWAQRz9MFzdDEqk"
+    };
     const next = () => {
         navigate("/")
     }
     const back = () => {
-        navigate("/games/"+id)
+        navigate("/games/" + id)
     }
-    const titulo = juegoTitulos[id] || "Título por defecto";
+
+    const titulo = juegoTitulos[id] || "Cargando titulo";
+    const videoSrc = videoUrls[id] || "";
 
     return (
         <div className="video-explicacion">
             <div className="video-explicacion__background" style={{ backgroundImage: `url(${backgrounds[backgroundIndex]})` }}>
                 <div className="video-explicacion__content">
                     <h1 className="video-explicacion__title">{titulo}</h1>
-                    <div className="video-explicacion__video">
-                     
+                    <div>
+                    <video
+                            title="video"
+                            width="560"
+                            height="315"
+                            src={videoSrc}
+                            autoPlay  
+                            loop
+                            controls
+                            controlsList="nodownload" 
+                        >
+                        </video>
                     </div>
                     <div className="video-explicacion__buttons">
                         <button className="video-explicacion__button" onClick={next}>Regresar</button>
-                        <button className="video-explicacion__button" style={{ backgroundColor: "#e4800e" } } onClick={back}>Continuar</button>
+                        <button className="video-explicacion__button" style={{ backgroundColor: "#e4800e" }} onClick={back}>Continuar</button>
                     </div>
                 </div>
             </div>
